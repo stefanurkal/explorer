c523bece67b45bddd0b6cf2a2be2f9fd9ccc8900
Stefanurkal <stephenfranada@gmail.com>
2025-04-07 21:10:13 -0700

    completed pass test plus added tests

--- src/ExplorerSearch.java
+++ src/ExplorerSearch.java
@@ -29,9 +29,82 @@ public class ExplorerSearch {
      * @return the number of spaces the explorer can reach
      */
     public static int reachableArea(int[][] island) {
-        // Implement your method here!
-        // Please also make more test cases
-        // I STRONGLY RECOMMEND testing some helpers you might make too
-        return -1;
+        int[] startingLocation = startLocation(island);
+
+        boolean[][] visited = new boolean[island.length][island[0].length];
+
+
+        return reachableArea(island, startingLocation, visited);
+    }
+
+    public static int[] startLocation (int [][] island) {
+        for(int r = 0; r < island.length;  r++ ){
+            for (int c = 0; c < island[0].length; c++){
+                if(island[r][c] == 0){
+                    return new int[]{r,c};
+                }
+            }
+        }
+        throw new IllegalArgumentException("No explorers are on the island");
+    }
+
+    public static List<int[]> possibleMoves(int[][] island, int[] current){
+        int curR = current[0];
+        int curC = current[1];
+
+        List<int[]> moves = new ArrayList<>();  
+        
+        //up
+        int newR = (curR -1);
+        int newC = (curC);
+        if( newR >= 0 && island[newR][newC] == 1){
+            moves.add(new int[]{newR, newC});
+        }
+        //down
+        newR = (curR + 1);
+        newC = (curC);
+        if ( newR < island.length && island[newR][newC] == 1){
+            moves.add(new int[]{newR, newC});
+        };
+        //left
+        newR = (curR);
+        newC = (curC - 1);
+        if(newC >= 0 && island[newR][newC] == 1){
+            moves.add(new int[]{newR, newC});
+        }
+        //right
+        newR = (curR);
+        newC = (curC + 1);
+        if(newC < island[0].length && island[newR][newC] == 1){
+            moves.add(new int[]{newR, newC});
+        }
+
+        return moves;
+
+
+    }
+
+    public static int reachableArea(int[][] island, int[] current, boolean[][] visited) {
+
+        // Base cases
+        int curR = current[0];
+        int curC = current[1];
+
+        if (visited[curR][curC]) return 0;
+
+        // Mark the locations as visited
+        visited[curR][curC] = true;
+
+       
+        int visitedAreasCount = 1;
+
+        
+        List<int[]> possibleMoves = possibleMoves(island, current);
+
+        for (int[] move : possibleMoves) {
+            visitedAreasCount += (reachableArea(island, move, visited));
+        }
+
+        return visitedAreasCount;
     }
 }
--- src/ExplorerSearchTest.java
+++ src/ExplorerSearchTest.java
@@ -1,4 +1,9 @@
 import static org.junit.Assert.*;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import org.junit.Test;
 
 public class ExplorerSearchTest {
@@ -17,4 +22,108 @@ public class ExplorerSearchTest {
 
     // Add more tests here!
     // Come up with varied cases
+
+    @Test
+    public void test_startInACrater() {
+        int[][] island = {
+            {1,1,1,3,1,1},
+            {3,2,3,3,3,1},
+            {1,1,3,0,3,3},
+            {3,1,3,3,3,1},
+            {1,1,1,2,1,1},
+        };
+        int actual = ExplorerSearch.reachableArea(island);
+        int[] location = {2, 3}; 
+
+        List<int[]> moves = ExplorerSearch.possibleMoves(island, location);
+        Set<String> moveSet = toSet(moves);
+        assertEquals(1, actual);
+        assertTrue(moveSet.isEmpty());
+    
+    }
+
+    @Test
+    public void test_startInAIslandOnanIsland() {
+        int[][] island = {
+            {1,1,1,3,1,1},
+            {3,2,2,2,2,1},
+            {1,1,2,2,2,3},
+            {3,1,2,2,2,1},
+            {1,1,1,2,1,1},
+        };
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            ExplorerSearch.startLocation(island);
+        });
+        assertEquals("No explorers are on the island", exception.getMessage());
+        
+    }
+
+
+    @Test
+    public void test_allDirections() {
+        int[][] island = {
+            {3,3,3,3,3,3},
+            {3,2,2,1,2,3},
+            {3,2,1,0,1,3},
+            {3,2,2,1,2,1},
+            {3,3,3,3,1,1},
+        };
+        int actual = ExplorerSearch.reachableArea(island);
+        int[] location = {2, 3}; 
+        List<int[]> moves = ExplorerSearch.possibleMoves(island, location);
+        Set<String> moveSet = toSet(moves);
+
+        assertEquals(5, actual);
+        
+        assertTrue(moveSet.contains("2,2"));
+        assertTrue(moveSet.contains("2,4"));
+        assertTrue(moveSet.contains("1,3"));
+        assertTrue(moveSet.contains("3,3"));
+    }
+
+    @Test 
+    public void test_outofbounds() {
+        int[][] island = {
+            {0,1,1,1,3,3},
+            {1,2,2,1,2,3},
+            {3,2,1,0,1,3},
+            {3,2,2,1,2,1},
+            {3,3,3,3,1,1},
+        };
+        
+        int[] location = {0, 0}; 
+        List<int[]> moves = ExplorerSearch.possibleMoves(island, location);
+        Set<String> moveSet = toSet(moves);
+       
+        assertFalse(moveSet.contains("-1,0"));
+        assertFalse(moveSet.contains("0, -1"));
+
+    }
+    @Test
+    public void test_outofbounds2() {
+        int[][] island = {
+            {1,1,1,1,3,3},
+            {1,2,2,1,2,3},
+            {3,2,1,0,1,3},
+            {3,2,2,1,2,1},
+            {3,3,3,3,1,0},
+        };
+       
+        int[] location = {4, 5}; 
+        List<int[]> moves = ExplorerSearch.possibleMoves(island, location);
+        Set<String> moveSet = toSet(moves);
+       
+        assertFalse(moveSet.contains("5,5"));
+        assertFalse(moveSet.contains("4, 6"));
+
+    }
+    private Set<String> toSet(List<int[]> list) {
+        Set<String> set = new HashSet<>();
+        for (int[] arr : list) {
+            set.add(arr[0] + "," + arr[1]);
+        }
+        return set;
+    }
 }
+
+
